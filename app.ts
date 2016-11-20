d0dd8a1d70f006e9329a7eea665dcae6c5628e98
kelvin knigton <keltheceo@gmail.com>
2016-11-19 16:07:13 -0800

    added price-display & product-image component

--- app.ts
+++ app.ts
@@ -1,44 +1,115 @@
-//@ProductsList: A component for rendering all ProductRows
-// and storing the currently selected product
+/*
+* @PriceDisplay: A component to show the price of a product
+*/
 
 @Component({
-  selector: 'products-list',
-  inputs: ['productList'],
-  outputs: ['onProductSelected'],
+    selector: 'price-display',
+    inputs: ['price'],
+    template: ` 
+        <div class="price-display"> \${{ price }} </div> 
+`
+})
+
+class PriceDisplay {
+    price: number
+}
+
+
+
+/*
+*  @ProductRow: A component for the view of single Product
+*/
+
+@Componet({
+  selector: 'product-row',
+  inputs: ['product'],
+  host: ['class': 'item'],
   template: `
-    <div class="ui items">
-        <product-row 
-            *ngFor="let myProduct of productList"
-            [product]="myProduct"
-            (click)='clicked(myProduct)'
-            [class.selected]="isSelected(myProduct)">
-        </product-row>
+    <product-image [product]="product"></product-image>
+    
+    <div class="content">
+        <div class="header">{{product.name}}</div>
+        <div class="meta">
+            <div class="product-sku">SKU #{{product.sku}}</div>
+        </div>
+        <div class="description">
+            <product-department [product]="product"></product-department>
+        </div>
     </div>
+    <price-display [price]="product.price"></price-display>
+    
   `
 })
 
-class ProductsList {
-  productList: Product[]
-
-  onProductSelected: EventEmitter<Product>
-
-  currentProduct: Product
-
-  constructor() {
-    this.onProductSelected = new EventEmitter()
-  }
-
-  clicked(product: Product): void {
-    this.currentProduct = product
-    this.onProductSelected.emit(product)
-  }
-
-  isSelected(product: Product): boolean {
-    if (!product || !this.currentProduct) {
-      return false
-    }
-    return product.sku === this.currentProduct.sku
-  }
-
+class ProductRow {
+  product: Product
 }
 
+
+/*
+*@ProductImage: A component to show a single Product's image
+*/
+
+@Component({
+    selector: 'product-image',
+    host: {class: 'ui small image'},
+    inputs: ['product'],
+    template: `
+        <img class='product-image' [src]='product.imageUrl'>
+    `
+})
+
+class ProductImage {
+    product: Product
+}
+
+
+
+
+
+
+
+// //@ProductsList: A component for rendering all ProductRows
+// // and storing the currently selected product
+//
+// @Component({
+//   selector: 'products-list',
+//   inputs: ['productList'],
+//   outputs: ['onProductSelected'],
+//   template: `
+//     <div class="ui items">
+//         <product-row
+//             *ngFor="let myProduct of productList"
+//             [product]="myProduct"
+//             (click)='clicked(myProduct)'
+//             [class.selected]="isSelected(myProduct)">
+//         </product-row>
+//     </div>
+//   `
+// })
+//
+// class ProductsList {
+//   productList: Product[]
+//
+//   onProductSelected: EventEmitter<Product>
+//
+//   currentProduct: Product
+//
+//   constructor() {
+//     this.onProductSelected = new EventEmitter()
+//   }
+//
+//   clicked(product: Product): void {
+//     this.currentProduct = product
+//     this.onProductSelected.emit(product)
+//   }
+//
+//   isSelected(product: Product): boolean {
+//     if (!product || !this.currentProduct) {
+//       return false
+//     }
+//     return product.sku === this.currentProduct.sku
+//   }
+//
+// }
+//
